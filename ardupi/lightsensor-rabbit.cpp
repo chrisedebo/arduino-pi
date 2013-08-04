85c81dce5c72f16dd76726475233a9d21fc9f786
Chris Nicel <chrisedebo@gmail.com>
2013-08-04 17:14:13 +0100

    Initial rabbitmq commit

--- ardupi/lightsensor-rabbit.cpp
+++ ardupi/lightsensor-rabbit.cpp
@@ -14,22 +14,20 @@ WirePi Wire;
 //Needed for SPI
 SPIPi SPI;
 
-// Values need for Steinhart-Hart equation
-// and calculating resistance.
-#define TENKRESISTOR 2940 //our 10K resistor
-#define BETA  3950// This is the Beta Coefficient of your thermistor
-#define THERMISTOR 2800 // The resistance of your thermistor at room
 
-//temperature
-#define ROOMTEMPK 298.15 
-//standard room temperature in Kelvin
-//(25 Celsius).
+#define RESISTOR 19800 //our 20K resistor
+#define LDR 5000 // The resistance of LDR
 
 // Number of readings to take
 // these will be averaged out to
 // get a more accurate reading
 // You can increase/decrease this as needed
 #define READINGS 7
+
+//Number of loops to make before displaying 
+//readings on console output
+#define LOOPS 10
+int LOOPCOUNTER = 0;
 
 /*********************************************************
 * IF YOUR ARDUINO CODE HAS OTHER FUNCTIONS APART FROM *
@@ -49,8 +47,9 @@ int main (){
 }
 
 void setup(void) {
-	printf("Starting up thermometer \n");
+	printf("Starting up lightsensor \n");
 	Wire.begin();
+	pinMode(2,OUTPUT); //set pin 2 on the sheild as an output
 }
 
 void loop(void) {
@@ -61,9 +60,6 @@ void loop(void) {
 	byte val1;
 
 	// Our temperature variables
-	float kelvin;
-	float fahrenheit;
-	float celsius;
 
 	int channelReading;
 	float analogReadingArduino;
@@ -81,23 +77,20 @@ void loop(void) {
 	7 0xFC
 	*******************/
 	
-	// 0xDC is our analog 0 pin
+	// 0x9C is our analog 1 pin
 	Wire.beginTransmission(8);
-	Wire.write(byte(0xDC));
+	Wire.write(byte(0x9C));
 	Wire.endTransmission();
 
 	/* Grab the two bytes returned from the
-	analog 0 pin, combine them
+	analog 1 pin, combine them
 	and write the value to the
 	combinedReadings array
 	*/
-
 	for(int r=0; r<READINGS; r++){
 		Wire.requestFrom(8,2);
 		val0 = Wire.read();
-		//printf("val0=%f ",int(val0));
 		val1 = Wire.read();
-		//printf("val1=%f \n",int(val1));
 		channelReading = int(val0)*16 + int(val1>>4);
 		analogReadingArduino = channelReading * 1023 /4095;
 		combinedReadings[r] = analogReadingArduino;
@@ -111,33 +104,31 @@ void loop(void) {
 	}
 	avResistance /= READINGS;
 
-	/* We can now calculate the resistance of
-	the readings that have come back from analog 0
-	*/
-	avResistance = (1023 / avResistance) - 1;
-	avResistance = TENKRESISTOR / avResistance;
-	resistance = avResistance / THERMISTOR;
+	//Turn LED on if low light levels
+	if (avResistance > 250){
+		digitalWrite(2,HIGH); //turn the LED on
+	}
+	else
+	{	
+		digitalWrite(2,LOW); //turn the LED off
+	}	
 
-	// Calculate the temperature in Kelvin
-	kelvin = log(resistance);
-	kelvin /= BETA;
-	kelvin += 1.0 / ROOMTEMPK;
-	kelvin = 1.0 / kelvin;
-	printf("Temperature in K ");
-	printf("%f \n",kelvin);
+	//Output to console every "LOOPS" loops
+	if (LOOPCOUNTER % LOOPS == 0) {
 
-	// Convert from Kelvin to Celsius
-	celsius = kelvin -= 273.15;
-	printf("Temperature in C ");
-	printf("%f \n",celsius);
-
-	// Convert from Celsius to Fahrenheit
-	fahrenheit = (celsius * 1.8) + 32;
-	printf("Temperature in F ");
-	printf("%f \n",fahrenheit);
-
-	// Three second delay before taking our next
+		printf("avresistance=%f \n",avResistance);
+		/* We can now calculate the resistance of
+		the readings that have come back from analog 0
+		*/
+		avResistance = (1023 / avResistance) - 1;
+		avResistance = RESISTOR / avResistance;
+		resistance = avResistance / LDR;
+		
+		printf("resistance=%f \n",resistance);
+	}
+	LOOPCOUNTER = LOOPCOUNTER + 1;
+	// 200 millisecond delay before taking our next
 	// reading
-	delay(3000);
+	delay(200);
 }
 
